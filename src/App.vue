aa95c5bace8d835c3d94f485aa0edb478f41009a
CV Charles Vukovic (####) <Charles.Vukovic@amgreetings.com>
2023-07-31 16:17:00 -0400

    holidays added

--- src/App.vue
+++ src/App.vue
@@ -38,6 +38,7 @@ export default {
 - make todays date special
 - indicate what site a holiday might link to
 - add to jenkins
+- make month things gray?
 -->
 <template>
     <!-- <h1>Calendar</h1> -->
